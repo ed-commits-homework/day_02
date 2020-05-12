11757be5b758dc32320f2b8019ad1fcbea9f8a03
ed-commits <edward.alexander.cant@gmail.com>
2020-05-12 11:34:21 +0100

    added if and elsif conditions

--- what_animal.rb
+++ what_animal.rb
@@ -1,2 +1,11 @@
 puts "What animal are you?"
 animal = gets.chomp()
+
+if animal == "shark"
+    puts "good animal"
+elsif animal == "pangolin"
+    puts "the worst animal"
+else
+    puts animal
+    puts "is not a good animal"
+end
